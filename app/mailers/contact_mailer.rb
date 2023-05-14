e872a4f518826d19a22f0d5deeb356f4a42757ec
sunkencity999 <theroyalart@gmail.com>
2023-05-14 01:05:43 -0700

    mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ApplicationMailer
-  default from: 'daemon@machinespiritcloud.com'
+  default from:'daemon@machinespiritcloud.com'
 
   def send_contact_email(name, email, message)
     @name = name
     @email = email
     @message = message
 
-    mail(to: 'ai@machinespiritclaude.com', subject: 'New Contact Message')
+    mail(to:'ai@machinespiritclaude.com', subject: 'New Contact Message')
   end
 end
