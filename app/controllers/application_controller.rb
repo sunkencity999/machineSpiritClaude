567b881d6a93ce44098c269aa7e0c67c8113115c
sunkencity999 <theroyalart@gmail.com>
2023-05-19 21:54:51 -0700

    attempted to add a loading screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
 before_action :configure_permitted_parameters, if: :devise_controller?
+before_action :show_loading_screen
 
 protected
 
@@ -9,6 +10,20 @@ def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:account_update, keys: [:name])
 end
 
+
+  private
+
+  def show_loading_screen
+    ActiveSupport::Notifications.subscribe(
+      'action_controller.render_template.start',
+      &method(:show_loading_screen_for_template)
+    )
+  end
+
+  def show_loading_screen_for_template(_name, _start, _finish, _exception)
+    # Show the loading screen
+    render :partial => 'layouts/loadingscreen'
+  end
 
 
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -41,3 +41,4 @@ function copyToClipboard(text) {
   document.body.removeChild(tempInput);
 }
 
+
